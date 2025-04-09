d47e7b7fa438930921ad68e68e4860b54abffb75
Buttery <84354123+m4rrk0121@users.noreply.github.com>
2025-04-09 19:49:49 +0100

    Update tokenDataService.js

--- backend/services/tokenDataService.js
+++ backend/services/tokenDataService.js
@@ -42,8 +42,8 @@ async function fetchAndStoreTokens() {
       const currentBlock = await provider.getBlockNumber();
       console.log(`Current block: ${currentBlock}`);
       
-      // Focus on the last 20,000 blocks
-      const START_BLOCK = Math.max(0, currentBlock - 20000);
+      // Focus on the last 2500 blocks
+      const START_BLOCK = Math.max(0, currentBlock - 2500);
       
       console.log(`Fetching events from block ${START_BLOCK} to ${currentBlock}`);
       
